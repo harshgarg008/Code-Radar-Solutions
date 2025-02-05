31c9b14b8f962fbe950a87b4df6966057542a589
harshgarg008 <harsh1319.becse24@chitkara.edu.in>
2025-02-05 20:45:59 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -1,18 +1,23 @@
 #include <stdio.h>
+
 int main() {
-    int a;
-    int b;
-    int c;
-    scanf("%d%d%d",&a,&b,&c);
-    if(a+b)
-    printf("%d\n",a+b);
-    else if(a-b)
-    printf("%d\n",a-b);
-    else if(a*b)
-    printf("%d\n",c);
-    else if(a / b == c)
-    printf("%d\n",a/b);
-    else 
-    printf("error\n");
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);  // Read input for a, b, and c
+
+    // Check if the sum of a and b equals c
+    if (a + b == c)
+        printf("%d\n", a + b);
+    // Check if the difference of a and b equals c
+    else if (a - b == c)
+        printf("%d\n", a - b);
+    // Check if the product of a and b equals c
+    else if (a * b == c)
+        printf("%d\n", a * b);
+    // Check if the division of a by b equals c (and b is not 0)
+    else if (b != 0 && a / b == c)
+        printf("%d\n", a / b);
+    else
+        printf("error\n");  // If no condition matches, print error
+
     return 0;
 }
